9036c0f5f64a3e8a7ee08f910c428d426747fec8
cz <chenze@.qqcom>
2024-06-18 22:43:02 +0800

    DataStructure-1.2.0

---
+++ src/main/java/com/chenze/DataStructure/tree/avltree/AVLTreeDemo.java
@@ -0,0 +1,322 @@
+package com.chenze.DataStructure.tree.avltree;
+
+public class AVLTreeDemo {
+    public static void main(String[] args) {
+        //int[] arr = {4,3,6,5,7,8};
+        //int[] arr = { 10, 12, 8, 9, 7, 6 };
+        int[] arr = { 10, 11, 7, 6, 8, 9 };
+        //创建一个 AVLTree对象
+        AVLTree avlTree = new AVLTree();
+        //添加结点
+        for (int j : arr) {
+            avlTree.add(new Node(j));
+        }
+
+        //遍历
+        System.out.println("中序遍历");
+        avlTree.infixOrder();
+
+        System.out.println("在平衡处理~~");
+        System.out.println("树的高度=" + avlTree.getRoot().height()); //3
+        System.out.println("树的左子树高度=" + avlTree.getRoot().leftHeight()); // 2
+        System.out.println("树的右子树高度=" + avlTree.getRoot().rightHeight()); // 2
+        System.out.println("当前的根结点=" + avlTree.getRoot());//8
+    }
+}
+class AVLTree{
+    private Node root;
+    public Node getRoot(){
+        return this.root;
+    }
+    //添加结点
+    public void add(Node node){
+        if (this.root==null){
+            this.root=node;
+        }else{
+            root.add(node);
+        }
+    }
+
+    //查找要删除的结点
+    public Node search(int value) {
+        if(root == null) {
+            return null;
+        } else {
+            return root.search(value);
+        }
+    }
+
+    //查找父结点
+    public Node searchParent(int value) {
+        if(root == null) {
+            return null;
+        } else {
+            return root.searchParent(value);
+        }
+    }
+
+    //编写方法:
+    //1. 返回的 以node 为根结点的二叉排序树的最小结点的值
+    //2. 删除node 为根结点的二叉排序树的最小结点
+    /**
+     *
+     * @param node 传入的结点(当做二叉排序树的根结点)
+     * @return 返回的 以node 为根结点的二叉排序树的最小结点的值
+     */
+    public int delRightTreeMin(Node node) {
+        Node target = node;
+        //循环的查找左子节点，就会找到最小值
+        while(target.left != null) {
+            target = target.left;
+        }
+        //这时 target就指向了最小结点
+        //删除最小结点
+        delNode(target.value);
+        return target.value;
+    }
+
+
+    // 删除结点
+    public void delNode(int value) {
+        if (root == null) {
+            return;
+        } else {
+            // 1.需求先去找到要删除的结点 targetNode
+            Node targetNode = search(value);
+            // 如果没有找到要删除的结点
+            if (targetNode == null) {
+                return;
+            }
+            // 如果我们发现当前这颗二叉排序树只有一个结点
+            if (root.left == null && root.right == null) {
+                root = null;
+                return;
+            }
+
+            // 去找到targetNode的父结点
+            Node parent = searchParent(value);
+            // 如果要删除的结点是叶子结点
+            if (targetNode.left == null && targetNode.right == null) {
+                // 判断targetNode 是父结点的左子结点，还是右子结点
+                if (parent.left != null && parent.left.value == value) { // 是左子结点
+                    parent.left = null;
+                } else if (parent.right != null && parent.right.value == value) {// 是由子结点
+                    parent.right = null;
+                }
+            } else if (targetNode.left != null && targetNode.right != null) { // 删除有两颗子树的节点
+                int minVal = delRightTreeMin(targetNode.right);
+                targetNode.value = minVal;
+
+            } else { // 删除只有一颗子树的结点
+                // 如果要删除的结点有左子结点
+                if (targetNode.left != null) {
+                    if (parent != null) {
+                        // 如果 targetNode 是 parent 的左子结点
+                        if (parent.left.value == value) {
+                            parent.left = targetNode.left;
+                        } else { // targetNode 是 parent 的右子结点
+                            parent.right = targetNode.left;
+                        }
+                    } else {
+                        root = targetNode.left;
+                    }
+                } else { // 如果要删除的结点有右子结点
+                    if (parent != null) {
+                        // 如果 targetNode 是 parent 的左子结点
+                        if (parent.left.value == value) {
+                            parent.left = targetNode.right;
+                        } else { // 如果 targetNode 是 parent 的右子结点
+                            parent.right = targetNode.right;
+                        }
+                    } else {
+                        root = targetNode.right;
+                    }
+                }
+
+            }
+
+        }
+    }
+
+
+    //中序遍历
+    public void infixOrder(){
+        if (this.root!=null){
+            this.root.infixOrder();
+        }else {
+            System.out.println("该数为空~");
+        }
+    }
+}
+class Node{
+    int value;
+    Node left;
+    Node right;
+
+    public Node(int value){
+        this.value=value;
+    }
+    @Override
+    public String toString() {
+        return "Node [value=" + value + "]";
+    }
+
+    //获取右子树的高度
+    public int rightHeight(){
+        if (right==null){
+            return 0;
+        }
+        return right.height();
+    }
+    //获取左子树的高度
+    public int leftHeight(){
+        if (left==null){
+            return 0;
+        }
+        return left.height();
+    }
+
+    //获取当前的树的高度
+    public int height(){
+        return Math.max(left==null?0:left.height(),right==null?0:right.height())+1;
+    }
+
+    //左旋转
+    public void leftRotate(){
+//        Node newNode = new Node(value);
+//        newNode.left=left;
+//        newNode.right=right.left;
+//        right=right.right;
+//        left=newNode;
+//        value=right.value;
+
+        //创建新的结点，以当前根结点的值
+        Node newNode = new Node(value);
+        //把新的结点的左子树设置成当前结点的左子树
+        newNode.left = left;
+        //把新的结点的右子树设置成带你过去结点的右子树的左子树
+        newNode.right = right.left;
+        //把当前结点的值替换成右子结点的值
+        //要先进行赋值，不然后面right会进行更改，值就会出现问题
+        value = right.value;
+        //把当前结点的右子树设置成当前结点右子树的右子树
+        right = right.right;
+        //把当前结点的左子树(左子结点)设置成新的结点
+        left = newNode;
+    }
+
+    //右旋转
+    public void rightRotate(){
+        Node newNode = new Node(value);
+        newNode.right = right;
+        newNode.left = left.right;
+        value = left.value;
+        left = left.left;
+        right = newNode;
+    }
+
+    //添加结点
+    public void add(Node node){
+        if (node == null) {
+            return;
+        }
+        // 判断传入的结点的值，和当前子树的根结点的值关系
+        if (node.value < this.value) {
+            // 如果当前结点左子结点为null
+            if (this.left == null) {
+                this.left = node;
+            } else {
+                // 递归的向左子树添加
+                this.left.add(node);
+            }
+        } else { // 添加的结点的值大于 当前结点的值
+            if (this.right == null) {
+                this.right = node;
+            } else {
+                // 递归的向右子树添加
+                this.right.add(node);
+            }
+
+        }
+
+        //双旋转和左旋
+        //若是右子树的高度是大于左子树的高度的
+        if (rightHeight()-leftHeight()>1){
+            if (right!=null&&right.leftHeight()>right.rightHeight()){
+                right.rightRotate();
+                leftRotate();
+            }else {
+                leftRotate();
+            }
+            return;
+        }
+
+        //双旋转和右旋
+        //若是左子树的高度大于右子树的高度
+        if (leftHeight()-rightHeight()>1){
+            if (left!=null&&left.rightHeight()>left.leftHeight()){
+                left.leftRotate();
+                rightRotate();
+            }else {
+                rightRotate();
+            }
+        }
+    }
+
+    //查找要删除的结点
+    /**
+     *
+     * @param value 希望删除的结点的值
+     * @return 如果找到返回该结点，否则返回null
+     */
+    public Node search(int value) {
+        if(value == this.value) { //找到就是该结点
+            return this;
+        } else if(value < this.value) {//如果查找的值小于当前结点，向左子树递归查找
+            //如果左子结点为空
+            if(this.left  == null) {
+                return null;
+            }
+            return this.left.search(value);
+        } else { //如果查找的值不小于当前结点，向右子树递归查找
+            if(this.right == null) {
+                return null;
+            }
+            return this.right.search(value);
+        }
+    }
+//查找要删除结点的父结点
+    /**
+     *
+     * @param value 要找到的结点的值
+     * @return 返回的是要删除的结点的父结点，如果没有就返回null
+     */
+    public Node searchParent(int value) {
+        //如果当前结点就是要删除的结点的父结点，就返回
+        if((this.left != null && this.left.value == value) ||
+                (this.right != null && this.right.value == value)) {
+            return this;
+        } else {
+            //如果查找的值小于当前结点的值, 并且当前结点的左子结点不为空
+            if(value < this.value && this.left != null) {
+                return this.left.searchParent(value); //向左子树递归查找
+            } else if (value >= this.value && this.right != null) {
+                return this.right.searchParent(value); //向右子树递归查找
+            } else {
+                return null; // 没有找到父结点
+            }
+        }
+
+    }
+
+    //中序遍历
+    public void infixOrder(){
+        if (this.left!=null){
+            this.left.infixOrder();
+        }
+        System.out.println(this.value);
+        if (this.right!=null){
+            this.right.infixOrder();
+        }
+    }
+}
